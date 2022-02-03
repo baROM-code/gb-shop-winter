01c0f877308dce151d529b0d15918ba2a1a8316a
Roman Bar <barom64@mail.ru>
2022-02-03 19:40:25 +0400

    Lesson_4_Barchenkov

--- gb-api/src/main/java/ru/gb/api/product/api/ProductGateway.java
+++ gb-api/src/main/java/ru/gb/api/product/api/ProductGateway.java
@@ -14,48 +14,48 @@ import java.util.List;
 @FeignClient(url = "http://127.0.0.1:8080/internal/api/v1/product", name = "ProductGateway")
 public interface ProductGateway {
 
-//    private final ProductService productService;
-//
-//    @GetMapping
-//    public List<ProductDto> getProductList() {
-//        return productService.findAll();
-//    }
-//
-//    @GetMapping("/info")
-//    public List<ProductManufacturerDto> getInfoProductList() {
-//        return productService.findAllInfo();
-//    }
-//
-//    @GetMapping("/{productId}")
-//    public ResponseEntity<?> getProduct(@PathVariable("productId") Long id) {
-//        ProductDto product;
-//        if (id != null) {
-//            product = productService.findById(id);
-//            if (product != null) {
-//                return new ResponseEntity<>(product, HttpStatus.OK);
-//            }
-//        }
-//        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//    }
-//
-//    @PostMapping
-//    public ResponseEntity<?> handlePost(@Validated @RequestBody ProductDto productDto) {
-//        ProductDto savedProduct = productService.save(productDto);
-//        HttpHeaders httpHeaders = new HttpHeaders();
-//        httpHeaders.setLocation(URI.create("/api/v1/product/" + savedProduct.getId()));
-//        return new ResponseEntity<>(httpHeaders, HttpStatus.CREATED);
-//    }
-//
-//    @PutMapping("/{productId}")
-//    public ResponseEntity<?> handleUpdate(@PathVariable("productId") Long id, @Validated @RequestBody ProductDto productDto) {
-//        productDto.setId(id);
-//        productService.save(productDto);
-//        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-//    }
-//
-//    @DeleteMapping("/{productId}")
-//    @ResponseStatus(HttpStatus.NO_CONTENT)
-//    public void deleteById(@PathVariable("productId") Long id) {
-//        productService.deleteById(id);
-//    }
+    private final ProductService productService;
+
+    @GetMapping
+    public List<ProductDto> getProductList() {
+        return productService.getProductList();
+    }
+
+    @GetMapping("/info")
+    public List<ProductManufacturerDto> getInfoProductList() {
+        return productService.getInfoProductList();
+    }
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<?> getProduct(@PathVariable("productId") Long id) {
+        ProductDto product;
+        if (id != null) {
+            product = productService.getProduct(id);
+            if (product != null) {
+                return new ResponseEntity<>(product, HttpStatus.OK);
+            }
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> handlePost(@Validated @RequestBody ProductDto productDto) {
+        ProductDto savedProduct = productService.handlePost(productDto);;
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(URI.create("/api/v1/product/" + productService.getProduct(productDto.getId())));
+        return new ResponseEntity<>(httpHeaders, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{productId}")
+    public ResponseEntity<?> handleUpdate(@PathVariable("productId") Long id, @Validated @RequestBody ProductDto productDto) {
+        productDto.setId(id);
+        productService.handleUpdate(id,productDto);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/{productId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable("productId") Long id) {
+        productService.deleteById(id);
+    }
 }
