cc7d736032fced2dbff923f2c13a9bb6d67b2a20
Roman Bar <barom64@mail.ru>
2022-03-09 20:18:46 +0400

    Lesson_11_Barchenkov

--- src/main/java/ru/gb/web/AuthController.java
+++ src/main/java/ru/gb/web/AuthController.java
@@ -9,8 +9,14 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.gb.api.security.dto.UserDto;
+import ru.gb.entity.security.AccountStatus;
+import ru.gb.entity.security.AccountUser;
 import ru.gb.service.UserService;
+import ru.gb.services.MailService;
+
+import java.util.Random;
 
 import javax.validation.Valid;
 
@@ -21,6 +27,8 @@ import javax.validation.Valid;
 public class AuthController {
 
     private final UserService userService;
+    private final MailService mailService;
+    private Integer cod;
 
     @GetMapping("/login")
     public String showLoginForm() {
@@ -35,6 +43,7 @@ public class AuthController {
 
     @PostMapping("/register")
     public String handleRegistration(@Valid UserDto userDto, BindingResult bindingResult, Model model) {
+
         String username = userDto.getUsername();
         log.info("Process registration form for: " + username);
         if (bindingResult.hasErrors()) {
@@ -46,14 +55,36 @@ public class AuthController {
             model.addAttribute("registrationError", "Пользователь с таким именем уже существует");
             log.info("Username {} already exists", username);
             return "auth/registration-form";
-        } catch (UsernameNotFoundException ignored) {}
+        } catch (UsernameNotFoundException ignored) {
+        }
 
         userService.register(userDto);
         log.info("Successfully created user with username: {}", username);
         model.addAttribute("username", username);
         // todo ДЗ 11 - добавить подтверждение email перед конечной активацией
         // todo сделать так чтобы аккаунт был создан но находился в статусе NOT_ACTIVE и enable=false до тех пор пока не введет на сайте пароль из мейла
+        cod = new Random().nextInt(55555);
+        mailService.sendMail(userDto.getEmail(), "Подтверждение регистрации на GB shop", "Код для подтверждения регистрации: " + cod);
+        AccountUser user = userService.findByUsername(username);
+        user.setStatus(AccountStatus.NOT_ACTIVE);
+        user.setEnabled(false);
+        userService.update(userDto);
         return "auth/registration-confirmation";
+    }
+
+    @PostMapping("/confirm")
+    public String handleConfirm(@RequestParam String incode, @RequestParam String username,
+                                     Model model, UserDto userDto) {
+        if (Integer.parseInt(incode) == cod) {
+            AccountUser user = userService.findByUsername(username);
+            user.setStatus(AccountStatus.ACTIVE);
+            user.setEnabled(true);
+            userService.update(userDto);
+        } else {
+            model.addAttribute("username", username);
+            return "auth/registration-confirmation";
+        }
+        return "redirect:/product/all";
     }
 
 }
